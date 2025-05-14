44dd61e171d9b7b8bfcce5bbfaf76005e96fb7f1
Heloisa31 <heloisa.souza.pinto@escola.pr.gov.br>
2025-05-14 08:17:41 -0300

    Update Vendedor.php

--- Vendedor.php
+++ Vendedor.php
@@ -5,4 +5,17 @@ class Vendedor
     public $nome;
     public $matricula;
     public $cpf;
+    public $telefone;
+
+    public function vender()
+    {
+        echo "O vendedor vendeu um produto";
+    }
+
+    public function entrega()
+    {
+        echo "O vendedor aceitou uma venda";
+    }
+
+
 }
